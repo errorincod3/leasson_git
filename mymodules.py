c14d99bdcee8939ef2f8e322181bb807fda8f136
Artem Pilipchuk <pilipchuk.artem@gmail.com>
2024-11-19 22:12:13 +0300

    add comment

--- mymodules.py
+++ mymodules.py
@@ -1,7 +1,9 @@
-#this is comment
+# Previously I had some complex logic in this
+# module, but I had to delete it due to reasons
+# Very sad!
 
 def my_function(first, second):
-    return first * second + 10
+    return first + second
 
 def another_function(first):
     return first + 1
